d20f931f6a7a0f05c426d01bb47dcd854022b9bf
punsupakorn <71259529+punsupakorn@users.noreply.github.com>
2021-09-13 16:38:00 +0700

    update

---
+++ config/firebase-config.js
@@ -0,0 +1,19 @@
+const admin = require("firebase-admin");
+const serviceAccount = require("./powercaregirl-vhuw-firebase-adminsdk-hfjd6-d87df09587.json")
+
+
+admin.initializeApp({
+    apiKey: "AIzaSyCDZzlYZjZBRiVe91UP6FGjP8waD5An9rI",
+    authDomain: "powercaregirl-vhuw.firebaseapp.com",
+    databaseURL: "https://powercaregirl-vhuw-default-rtdb.firebaseio.com",
+    projectId: "powercaregirl-vhuw",
+    storageBucket: "powercaregirl-vhuw.appspot.com",
+    messagingSenderId: "302094464999",
+    appId: "1:302094464999:web:9e3fceeb50c69039eb6f42",
+    measurementId: "G-B8Q3BVT3MH",
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://powercaregirl-vhuw-default-rtdb.firebaseio.com",
+  });
+  
+  const db = admin.firestore();
+  module.exports = db ;

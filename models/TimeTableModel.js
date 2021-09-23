841ad2522a9d6c64893b9bef6aa94d5b57f0021c
Supakorn Songhong <Supakorn.pun@gmail.com>
2021-09-23 14:04:59 +0700

    update

--- models/TimeTableModel.js
+++ models/TimeTableModel.js
@@ -1,8 +1,14 @@
 const db = require("../config/firebase-config");
 
 ///// get timetable /////
-const getTimeTable = () => {
+const AddTimeTable = () =>{
+    const TimeSlotArr = [08.30-9.00,]
+    const TimeTableRef = db.collection("TimeTable").doc().set({
+
+    })
+}
+// const getTimeTable = () => {
     
-};
+// };
 
 module.exports = getTimeTable;

3524a5b01361c42f48e07d7949be6530f0c95731
Supakorn Songhong <Supakorn.pun@gmail.com>
2021-10-06 00:30:05 +0700

    update

--- controllers/AppointmentController.js
+++ controllers/AppointmentController.js
@@ -1,15 +1,14 @@
 const { getTimeTable } = require("../models/TimeTableModel");
 const { addAppointment } = require("../models/AppointmentModel");
-module.exports = { getTimeTable, addAppointment };
 
 const createAppointment = async (req, res) => {
   try {
+    const UserID = req.body.UserID;
+    const DoctorName = req.body.DoctorName;
     const Symptom = req.body.Symptom;
     const Date = req.body.Date;
-    const DoctorName = req.body.DoctorName;
     const Time = req.body.Time;
-
-    await addAppointment(Symptom, Date, DoctorName, Time);
+    await addAppointment(UserID, DoctorName, Symptom, Date, Time);
     res.status(200).send("created appointment !");
   } catch (error) {
     res.status(500).json(error);
@@ -17,4 +16,4 @@ const createAppointment = async (req, res) => {
   }
 };
 
-module.exports = { createAppointment };
+module.exports = { createAppointment, getTimeTable };
--- models/AppointmentModel.js
+++ models/AppointmentModel.js
@@ -1,17 +1,30 @@
 const db = require("../config/firebase-config");
 
 ///// crate /////
-const addAppointment = async (Symptom, Date, DoctorName, Time) => {
+const addAppointment = async (UserID, DoctorName, Symptom, Date, Time) => {
   const appointmentRef = db.collection("Appointment").doc();
-
   await db.collection("Appointment").doc(appointmentRef.id).set({
     AppointmentID: appointmentRef.id,
-    Symptom: Symptom,
-    Date: Date,
+    UserID: UserID,
     DoctorName: DoctorName,
+    Intitial_Symptoms: Symptom,
+    Date: Date,
     Time: Time,
   });
 };
+
+// const addAppointment = async (Symptom, Date, DoctorName, Time, Patient) => {
+//   const appointmentRef = db.collection("Appointment").doc();
+
+//   await db.collection("Appointment").doc(appointmentRef.id).set({
+//     AppointmentID: appointmentRef.id,
+//     Symptom: Symptom,
+//     Date: Date,
+//     DoctorName: DoctorName,
+//     Time: Time,
+//     Patient: Patient,
+//   });
+// };
 
 ///// read /////
 

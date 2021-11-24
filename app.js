4d2e43d45385e867ec194be7b43c313d63890f01
Darispb <daris.pb@mail.kmutt.ac.th>
2021-11-24 15:17:49 +0700

    update

--- app.js
+++ app.js
@@ -30,9 +30,9 @@ app.use("/AppointmentConfirm", appointmentConfirmRouter);
 app.use("/EditUserProfile", editUserProfileRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+// app.use(function (req, res, next) {
+//   next(createError(404));
+// });
 
 // // error handler
 // app.use(function(err, req, res, next) {

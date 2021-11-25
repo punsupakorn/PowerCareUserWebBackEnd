6a78cde4fcd77193be52aa35e06edb8a3521b65e
mook22 <panita.22@mail.kmutt.ac.th>
2021-11-25 17:52:25 +0700

    push appointment

--- linepushmessage/pushmessage.js
+++ linepushmessage/pushmessage.js
@@ -5,137 +5,190 @@ const client = new line.Client({
     "tDcCgW6YfJUhQ15V/Oii2uzvvIqEXUE/3xsEuAtIYeKWTMF+4CbtY77ohXp6Ry9Q/mhHO1r212CrHvUokkxk28P1W5sQdBA6dJ4jeH/4OUHbDbxRhl6EFuHsu0e6TZoNHQg7GZZzI0A8hk9BTzrUMgdB04t89/1O/w1cDnyilFU=",
 });
 
-const message = (name) => {
+const AppointmentConfirm = (userName, initial_Symptoms, date, time, doctorName, status) => {
   return [
     {
-      type: "flex",
-      altText: "This is flex messages",
-      contents: {
-        type: "bubble",
-        hero: {
-          type: "image",
-          url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
-          size: "full",
-          aspectRatio: "20:13",
-          aspectMode: "cover",
-          action: {
-            type: "uri",
-            uri: "http://linecorp.com/",
+          type: "flex",
+          altText: "คำขอการทำนัด",
+          contents: {
+            "type": "bubble",
+            "direction": "ltr",
+            "body": {
+              "type": "box",
+              "layout": "vertical",
+              "contents": [
+                {
+                  "type": "text",
+                  "text": "คำขอการทำนัด",
+                  "weight": "bold",
+                  "size": "xl",
+                  "align": "start",
+                  "contents": []
+                },
+                {
+                  "type": "box",
+                  "layout": "vertical",
+                  "spacing": "sm",
+                  "margin": "lg",
+                  "contents": [
+                    {
+                      "type": "box",
+                      "layout": "baseline",
+                      "spacing": "sm",
+                      "contents": [
+                        {
+                          "type": "text",
+                          "text": "ชื่อ",
+                          "size": "md",
+                          "color": "#AAAAAA",
+                          "flex": 1,
+                          "contents": []
+                        },
+                        {
+                          "type": "text",
+                          "text": `${userName}`,
+                          "size": "md",
+                          "color": "#666666",
+                          "flex": 2,
+                          "wrap": true,
+                          "contents": []
+                        }
+                      ]
+                    },
+                    {
+                      "type": "box",
+                      "layout": "baseline",
+                      "spacing": "sm",
+                      "contents": [
+                        {
+                          "type": "text",
+                          "text": "อาการ",
+                          "size": "md",
+                          "color": "#AAAAAA",
+                          "flex": 1,
+                          "contents": []
+                        },
+                        {
+                          "type": "text",
+                          "text": `${initial_Symptoms}`,
+                          "size": "md",
+                          "color": "#666666",
+                          "flex": 2,
+                          "wrap": true,
+                          "contents": []
+                        }
+                      ]
+                    },
+                    {
+                      "type": "box",
+                      "layout": "baseline",
+                      "spacing": "sm",
+                      "contents": [
+                        {
+                          "type": "text",
+                          "text": "วัน",
+                          "size": "md",
+                          "color": "#AAAAAA",
+                          "flex": 1,
+                          "align": "start",
+                          "contents": []
+                        },
+                        {
+                          "type": "text",
+                          "text": `${date}`,
+                          "size": "md",
+                          "color": "#666666",
+                          "flex": 2,
+                          "wrap": true,
+                          "contents": []
+                        }
+                      ]
+                    },
+                    {
+                      "type": "box",
+                      "layout": "baseline",
+                      "contents": [
+                        {
+                          "type": "text",
+                          "text": "เวลา",
+                          "size": "md",
+                          "color": "#AAAAAA",
+                          "flex": 1,
+                          "align": "start",
+                          "contents": []
+                        },
+                        {
+                          "type": "text",
+                          "text": `${time}`,
+                          "size": "md",
+                          "color": "#666666",
+                          "flex": 2,
+                          "wrap": true,
+                          "contents": []
+                        }
+                      ]
+                    },
+                    {
+                      "type": "box",
+                      "layout": "baseline",
+                      "contents": [
+                        {
+                          "type": "text",
+                          "text": "ชื่อแพทย์",
+                          "size": "md",
+                          "color": "#AAAAAA",
+                          "flex": 1,
+                          "contents": []
+                        },
+                        {
+                          "type": "text",
+                          "text": `${doctorName}`,
+                          "size": "md",
+                          "color": "#666666",
+                          "flex": 2,
+                          "wrap": true,
+                          "contents": []
+                        }
+                      ]
+                    },
+                    {
+                      "type": "separator"
+                    },
+                    {
+                      "type": "box",
+                      "layout": "baseline",
+                      "contents": [
+                        {
+                          "type": "text",
+                          "text": "สถานะ",
+                          "size": "md",
+                          "color": "#AAAAAA",
+                          "flex": 1,
+                          "contents": []
+                        },
+                        {
+                          "type": "text",
+                          "text": `${status}`,
+                          "size": "md",
+                          "color": "#666666",
+                          "flex": 2,
+                          "wrap": true,
+                          "contents": []
+                        }
+                      ]
+                    }
+                  ]
+                }
+              ]
+            }
           },
-        },
-        body: {
-          type: "box",
-          layout: "vertical",
-          contents: [
-            {
-              type: "text",
-              text: `${name}`,
-              weight: "bold",
-              size: "xl",
-            },
-            {
-              type: "box",
-              layout: "baseline",
-              margin: "md",
-              contents: [
-                {
-                  type: "icon",
-                  size: "sm",
-                  url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
-                },
-                {
-                  type: "icon",
-                  size: "sm",
-                  url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
-                },
-                {
-                  type: "icon",
-                  size: "sm",
-                  url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
-                },
-                {
-                  type: "icon",
-                  size: "sm",
-                  url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
-                },
-                {
-                  type: "icon",
-                  size: "sm",
-                  url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png",
-                },
-                {
-                  type: "text",
-                  text: "4.0",
-                  size: "sm",
-                  color: "#999999",
-                  margin: "md",
-                  flex: 0,
-                },
-              ],
-            },
-            {
-              type: "box",
-              layout: "vertical",
-              margin: "lg",
-              spacing: "sm",
-              contents: [
-                {
-                  type: "box",
-                  layout: "baseline",
-                  spacing: "sm",
-                  contents: [
-                    {
-                      type: "text",
-                      text: "Place",
-                      color: "#aaaaaa",
-                      size: "sm",
-                      flex: 1,
-                    },
-                    {
-                      type: "text",
-                      text: "Miraina Tower, 4-1-6 Shinjuku, Tokyo",
-                      wrap: true,
-                      color: "#666666",
-                      size: "sm",
-                      flex: 5,
-                    },
-                  ],
-                },
-                {
-                  type: "box",
-                  layout: "baseline",
-                  spacing: "sm",
-                  contents: [
-                    {
-                      type: "text",
-                      text: "Time",
-                      color: "#aaaaaa",
-                      size: "sm",
-                      flex: 1,
-                    },
-                    {
-                      type: "text",
-                      text: "10:00 - 23:00",
-                      wrap: true,
-                      color: "#666666",
-                      size: "sm",
-                      flex: 5,
-                    },
-                  ],
-                },
-              ],
-            },
-          ],
-        },
-      },
-    },
-  ];
+        }
+  ]
 };
 
 
-
 // console.log("start")
-// client.pushMessage('U3469bee1028c5f4c2e16199bb53ec5b7', message("Appointment"))
+// client.pushMessage('Ue28402bc40f682c854562028a4df4040', message("Appointment"))
 //     .then(() => {
 //         console.log('done')
 //     })
@@ -144,4 +197,4 @@ const message = (name) => {
 //         console.log(err)
 //     });
 
-module.exports = { client, message };
+module.exports = { client, AppointmentConfirm };

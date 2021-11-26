7d1f2107d09a0f522580f8577071f7cbe3bab11d
Supakorn Songhong <Supakorn.pun@gmail.com>
2021-11-26 15:19:06 +0700

    up

--- routes/PostponeConfirmRoute.js
+++ routes/PostponeConfirmRoute.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { editAppointment } = require("../controllers/PostponeConfirmController");
 
-router.put("/", async (req, res) => {
+router.post("/", async (req, res) => {
   const {
     AppointmentID,
     OldTimeTableID,

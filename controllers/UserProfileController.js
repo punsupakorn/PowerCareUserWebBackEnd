b5c7afacc723adc6a08aaaeffae57c0f7d3ecb04
Supakorn Songhong <Supakorn.pun@gmail.com>
2021-11-29 03:37:19 +0700

    update

--- controllers/UserProfileController.js
+++ controllers/UserProfileController.js
@@ -1,16 +1,16 @@
 const { addUser, checkUser } = require("../models/UserModel");
 
-const createUser = async (req, res) => {
+const createUser = async (
+  FirstName,
+  LastName,
+  DateOfBirth,
+  Sex,
+  Address,
+  Phone,
+  Email,
+  accessToken
+) => {
   try {
-    const FirstName = req.body.FirstName;
-    const LastName = req.body.LastName;
-    const DateOfBirth = req.body.DateOfBirth;
-    const Sex = req.body.Sex;
-    const Address = req.body.Address;
-    const Phone = req.body.Phone;
-    const Email = req.body.Email;
-    const accessToken = req.body.accessToken;
-
     await addUser(
       FirstName,
       LastName,
--- routes/UserProfileRoute.js
+++ routes/UserProfileRoute.js
@@ -2,6 +2,27 @@ const express = require("express");
 const router = express.Router();
 const { createUser } = require("../controllers/UserProfileController");
 
-router.post("/", createUser);
+router.post("/", async (req, res) => {
+  const FirstName = req.body.FirstName;
+  const LastName = req.body.LastName;
+  const DateOfBirth = req.body.DateOfBirth;
+  const Sex = req.body.Sex;
+  const Address = req.body.Address;
+  const Phone = req.body.Phone;
+  const Email = req.body.Email;
+  const accessToken = req.body.accessToken;
+
+  let result = await createUser(
+    FirstName,
+    LastName,
+    DateOfBirth,
+    Sex,
+    Address,
+    Phone,
+    Email,
+    accessToken
+  );
+  res.send(result);
+});
 
 module.exports = router;
